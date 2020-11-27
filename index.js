759ee0429c62086b833b34d84bffeb094d318e1c
buquio <kehinde.bukola14@gmail.com>
2020-11-27 08:54:20 +0100

    updared test.html

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-const downButton = document.querySelector('#down')
-const upButton = document.querySelector('#up')
-const buyListDisplay = document.querySelector('#first-list')
-const fridgeListDisplay = document.querySelector('#second-list')
+let buyListDisplay;
+let fridgeListDisplay;
 
+window.onload = function(){
+    const downButton = document.querySelector('#down')
+    const upButton = document.querySelector('#up')
+    buyListDisplay = document.querySelector('#first-list')
+    fridgeListDisplay = document.querySelector('#second-list')
+    
+    upButton.addEventListener('click', moveUp)
+    downButton.addEventListener('click', moveDown)
+    }
+    
 const buyList = ['almond milk', 'avocado', 'cheese', 'halloumi' ]
-const fridge = []
+const fridge = [] //fridgelist
 
-//Challenge: Please fill the fridge with 5 items of your choice. 
+//Challenge1: Please fill the fridge with 5 items of your choice. 
 fridge.push('onion', 'salad', 'chocolate', 'crisps', 'egg')
 
 
 //Challenge 2: You have bought some almond milk. Please remove it from the
 //buyList and add it to the items in fridge.
-let item = buyList.shift()
-fridge.unshift(item)
+// let item = buyList.shift()
+// fridge.unshift(item)
+// or
+for(let i=0; i<=buyList.length; i++) {
+    if(buyList[i]=== "almond milk"){
+        // buyList[i]=fridge.unshift(buyList[i])
+        buyList[i]=fridge.push(buyList[i])
+
+    }
+}
+// or  
+// let item = buyList[0]
+// fridge[0]=item
 
 
-//BONUS 1:Write a function that will remove an item from the fridge, and put it in the
-//buyList, on the press of the moveUp button.
+//BONUS 1:Write a function that will remove an item from the fridge 
+// and put it in the buyList, on the press of the moveUp button.
 
 function moveUp(){
     //your code
@@ -26,8 +45,6 @@ function moveUp(){
     buyListDisplay.innerHTML = buyList
     fridgeListDisplay.innerHTML = fridge
 }
-
-upButton.addEventListener('click', moveUp)
 
 //BONUS 2:Write a function that will remove the last item in the buyList, 
 //and put it in the fridge.
@@ -40,7 +57,7 @@ function moveDown(){
     fridgeListDisplay.innerHTML = fridge
 }
 
-downButton.addEventListener('click', moveDown)
 
-buyListDisplay.innerHTML = buyList
-fridgeListDisplay.innerHTML = fridge
+
+// buyListDisplay.innerHTML = buyList
+// fridgeListDisplay.innerHTML = fridge
